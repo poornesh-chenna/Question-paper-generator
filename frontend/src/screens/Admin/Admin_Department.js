bc209310a9881043e3eebef8da84c5bfc54b8e06
LeelaRohith <leelarohith10@gmail.com>
2023-02-24 17:01:51 +0530

    facultydashboard

--- frontend/src/screens/Admin/Admin_Department.js
+++ frontend/src/screens/Admin/Admin_Department.js
@@ -1,42 +1,42 @@
-import navWrapper from '../../components/Navbar'
-import * as React from 'react'
-import { styled } from '@mui/material/styles'
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
+import navWrapper from "../../components/Navbar";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 
-import { useNavigate } from 'react-router-dom'
-import Adddepartment from '../../components/Adddepartment'
-import { useEffect } from 'react'
-import { Axios } from '../../utils/Axios'
-import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import Adddepartment from "../../components/Adddepartment";
+import { useEffect } from "react";
+import { Axios } from "../../utils/Axios";
+import { useState } from "react";
 
 function AdminDepartment() {
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
-  }))
+  }));
   const [newdepartment, setnewDepartment] = useState({
-    deptname: '',
-    dept: '',
-  })
-  const [toggle, setToggle] = useState(false)
+    deptname: "",
+    dept: "",
+  });
+  const [toggle, setToggle] = useState(false);
 
-  const [departments, setdepartments] = useState(null)
+  const [departments, setdepartments] = useState(null);
   useEffect(() => {
     const fetchDepts = async () => {
-      const depts = await Axios.get('/departments')
-      setdepartments(depts.data)
-    }
-    fetchDepts()
-  }, [toggle])
+      const depts = await Axios.get("/departments");
+      setdepartments(depts.data);
+    };
+    fetchDepts();
+  }, [toggle]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <Box sx={{ width: '70%' }}>
+    <Box sx={{ width: "70%" }}>
       {
         <Adddepartment
           department={newdepartment}
@@ -50,9 +50,9 @@ function AdminDepartment() {
         {departments &&
           departments.map((item, index) => {
             return (
-              <Grid key={index} item xs={6} sx={{ width: '50px' }}>
+              <Grid key={index} item xs={6} sx={{ width: "50px" }}>
                 <Item
-                  sx={{ padding: '20px', fontSize: '16px' }}
+                  sx={{ padding: "20px", fontSize: "16px" }}
                   onClick={() =>
                     navigate(item.dept, {
                       state: {
@@ -67,23 +67,11 @@ function AdminDepartment() {
                   {item.deptname}
                 </Item>
               </Grid>
-            )
+            );
           })}
-        {/* <Grid item xs={6}>
-        <Item onClick={()=>navigate('/CSE')}>CSE</Item>
-      </Grid>
-      <Grid item xs={6}>
-        <Item>ECE</Item>
-      </Grid>
-      <Grid item xs={6}>
-        <Item>EEE</Item>
-      </Grid>
-      <Grid item xs={6}>
-        <Item>MECH</Item>
-      </Grid> */}
       </Grid>
     </Box>
-  )
+  );
 }
 
-export const dept = navWrapper(<AdminDepartment />)
+export const dept = navWrapper(<AdminDepartment />);
