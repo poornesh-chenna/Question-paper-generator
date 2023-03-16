1fab43f596d8b713e14dcbb53072c80cb1b22715
poornesh <poorneshchenna29@gmail.com>
2023-03-16 11:20:51 +0530

    formatted date

--- backend/routers/questions.router.js
+++ backend/routers/questions.router.js
@@ -9,58 +9,22 @@ import HtmlDocx from 'html-docx-js'
 import fs from 'fs'
 
 const router = express.Router()
-const htmlContent = `
-  <html>
-    <head>
-      <title>My HTML to Word Document</title>
-    </head>
-    <body>
-       <div>
-        <div style="line-height: 1">
-        <p align="center">
-          <span>G.PULLA REDDY ENGINEERING COLLEGE(AUTONOMOUS): KURNOOL</span>
-          <br/>
-          <span>B.TECH V SEMESTER</span>
-          <br/>
-          <span>SECOND SESSIONAL EXAMINATION NOVEMBER-2022</span>
-          <br/>
-          <span>DEspanARTMENT OF COMspanUTER SCIENCE AND ENGINEERING</span>
-          <br/>
-          <span>ARTIFICIAL INTELLIGENCE (AI)</span>
-          <br/>
-          <span>COMMON FOR CSE & CST</span>
-          <br/>
-          <span>(SCHEME-2020)</span>
-          </p>
-        </div>
-        <div>Time :</div>
-        <div>Date : </div>
-        <div style= "text-align: right;">Max Marks : 25</div>
-        <p style= "text-align: center;">Section - 1</p>
-       
-        <table style="width:100%;">
-<tr style="width:100%; ">
-<td style="width:80%;">1a. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</td>
-<td align="right" style="padding-right:40px">6M</td>
-</tr>
-<tr style="width:100%">
-<td>1a. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</td>
-<td align="right" style="padding-right:40px">6M</td>
-</tr>
-</table>
-      </div>
-    </body>
-  </html>
-`
+
 // const htmlContent = fs.readFileSync("input.html", "utf-8");
 
 // Convert the HTML file to a Word document
 router.post('/convertToWord', authorizeUser, async (req, res) => {
   const wordFile = HtmlDocx.asBlob(req.body.htmlContent)
   const wordDoc = await wordFile.arrayBuffer()
-  // Write the Word file to disk
 
-  const fileName = Date.now() + 'output.docx'
+  // formatting date
+  const timestamp = Date.now()
+  const date = new Date(timestamp)
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  const dateString = date.toLocaleDateString('en-US', options)
+
+  // Write the Word file to disk
+  const fileName = dateString + 'output.docx'
   const file = fs.writeFileSync(fileName, Buffer.from(wordDoc))
   // console.log(fileName)
   const recentWord = fs.readFileSync(fileName, 'base64')
